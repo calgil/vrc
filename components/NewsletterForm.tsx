cc2c617b45ef7fd85f8f9bdc69a52480a1313be2
calgil <clgilbert1105@gmail.com>
2023-03-22 10:10:22 -0600

    integrate convertkit with newsletter form

--- components/NewsletterForm.tsx
+++ components/NewsletterForm.tsx
@@ -1,5 +1,7 @@
 import s from "../styles/components/NewsletterForm.module.scss";
 import classNames from "classnames/bind";
+import { useState } from "react";
+import { isEmailValid } from "@/utils/isEmailValid";
 
 let cx = classNames.bind(s);
 
@@ -9,11 +11,22 @@ type NewsletterFormProps = {
   inHero: boolean;
 };
 
+export type FormData = {
+  name: string;
+  email: string;
+};
+
 export const NewsletterForm = ({
   heading,
   description,
   inHero,
 }: NewsletterFormProps) => {
+  const [name, setName] = useState<string>("");
+  const [nameError, setNameError] = useState<boolean>(false);
+  const [email, setEmail] = useState<string>("");
+  const [emailError, setEmailError] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+
   let newsletterClass = cx({
     newsletter: true,
     inHero,
@@ -23,22 +36,99 @@ export const NewsletterForm = ({
     formContainer: true,
     inHero,
   });
+
+  let nameInputClass = cx({
+    input: true,
+    error: nameError,
+  });
+
+  let emailInputClass = cx({
+    input: true,
+    error: emailError,
+  });
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!isEmailValid(e.target.value)) {
+      return;
+    }
+    setEmail(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // this sucks figure it out!!
+    // if (!name) {
+    //   setMessage("Name is required");
+    //   return setNameError(true);
+    // }
+    // if (!email) {
+    //   setMessage("Email is required");
+    //   return setEmailError(true);
+    // }
+    // if (name) {
+    //   setNameError(false);
+    // }
+    // if (email) {
+    //   setEmailError(false);
+    // }
+    // if (name && email) {
+    //   setMessage("");
+    // }
+    console.log("subscribe", { name, email });
+
+    fetch(
+      `${process.env.CONVERTKIT_BASE_URL}/${process.env.CONVERTKIT_FORM_ID}/subscribe`,
+      {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json; charset=utf-8",
+        },
+        body: JSON.stringify({
+          api_key: `${process.env.CONVERTKIT_API_KEY}`,
+          email: email,
+          first_name: name,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error(error));
+    setName("");
+    setNameError(false);
+    setEmail("");
+    setEmailError(false);
+  };
   return (
     <div className={newsletterClass}>
       <h4 className={s.newsletterTitle}>{heading}</h4>
       <p className={s.description}>{description}</p>
-      <form
-        className={formClass}
-        onSubmit={(e) => {
-          e.preventDefault();
-          console.log("subscribe to newsletter");
-        }}
-      >
+      <form className={formClass} onSubmit={handleSubmit}>
         <div className={s.inputContainer}>
-          <input className={s.input} placeholder="Your Name" />
-          <input className={s.input} placeholder="Email Address" />
+          <input
+            className={nameInputClass}
+            name="name"
+            type="text"
+            value={name}
+            // required
+            placeholder="Your Name"
+            onChange={handleNameChange}
+          />
+          <input
+            className={emailInputClass}
+            name="email"
+            type="email"
+            value={email}
+            // required
+            placeholder="Email Address"
+            onChange={handleEmailChange}
+          />
         </div>
         <input className={s.submitBtn} type="submit" value="Submit" />
+        {message && <p className={s.errorMsg}>{message}</p>}
       </form>
     </div>
   );
