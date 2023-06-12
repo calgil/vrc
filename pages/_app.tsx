ff5d8a0f1a5a2a1f571dedafc8795b470c93ab50
calgil <clgilbert1105@gmail.com>
2023-06-12 11:57:04 -0600

    import font awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
 import { Layout } from "@/components/Layout";
-import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import { Analytics } from "@vercel/analytics/react";
+import "@/styles/globals.scss";
+import { config } from "@fortawesome/fontawesome-svg-core";
+
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
