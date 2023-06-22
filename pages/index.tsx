128c03283f300553a99aa9df4b5a9f3e41955898
calgil <clgilbert1105@gmail.com>
2023-06-22 07:45:48 -0600

    create linkcards component

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,41 +9,9 @@ import { faPhone, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { EmergencyContact } from "@/components/EmergencyContact";
 import Link from "next/link";
-
-type LinkCard = {
-  url: string;
-  bgUrl: string;
-  bgColor: string;
-  text: string;
-};
+import { LinkCards } from "@/components/LinkCards";
 
 export default function Home() {
-  const LinkCards: LinkCard[] = [
-    {
-      url: "/services#emergency",
-      bgUrl: "/cardBg/emergencyBg.webp",
-      bgColor: "rgba(154, 48, 64, 0.85)",
-      text: "Emergency Services",
-    },
-    {
-      url: "/services",
-      bgUrl: "/cardBg/specialtyBg.webp",
-      bgColor: "rgba(111, 113, 168, 0.85)",
-      text: "Specialty Service",
-    },
-    {
-      url: "/veterinarians",
-      bgUrl: "/cardBg/vets.webp",
-      bgColor: "rgba(37, 37, 37, 0.85)",
-      text: "For Referring Veterinarians",
-    },
-    {
-      url: "/veterinarians",
-      bgUrl: "/cardBg/owners.webp",
-      bgColor: "rgba(1, 109, 118, 0.85)",
-      text: "For Pet Owners",
-    },
-  ];
   return (
     <>
       <Head>
@@ -64,23 +32,7 @@ export default function Home() {
           bgUrl="/heroBg/homeBg.webp"
         />
         <EmergencyContact />
-        <div className={s.linkCards}>
-          {LinkCards.map((card) => (
-            <Link className={s.link} key={card.text} href={card.url}>
-              <div
-                className={s.linkCard}
-                style={{
-                  background: ` linear-gradient(
-                    ${card.bgColor}, 
-                    ${card.bgColor}
-                  ), url(${card.bgUrl}) no-repeat center / cover`,
-                }}
-              >
-                <p className={s.cardText}>{card.text}</p>
-              </div>
-            </Link>
-          ))}
-        </div>
+        <LinkCards />
         {/* <OurStory /> */}
         {/* <CoreValues /> */}
         {/* <h2 className={s.sectionHeader}>Join the North Springs VRC Family!</h2>
