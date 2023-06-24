8d151b74ef262f42d0dd8532765cc890a6a4c2c0
calgil <clgilbert1105@gmail.com>
2023-06-24 07:41:55 -0600

    fix navbar link

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -27,7 +27,7 @@ export const Navbar = () => (
         </a>
         <a
           className={`${s.btn} ${s.findBtn}`}
-          href="https://www.google.com/maps/place/10520+White+Diamond+Pt,+Colorado+Springs,+CO+80908/@38.9838331,-104.7985129,17z/data=!3m1!4b1!4m6!3m5!1s0x87134c52f7f498d5:0xadc8e3a53ba07389!8m2!3d38.983829!4d-104.795938!16s%2Fg%2F11ssq6ysqz?entry=ttu"
+          href="https://www.google.com/maps/place/North+Springs+Veterinary+Referral+Center/@38.9838331,-104.7985129,17z/data=!3m1!4b1!4m6!3m5!1s0x87134d111065c23d:0xaab025eb0119a0ea!8m2!3d38.983829!4d-104.795938!16s%2Fg%2F11kq2mdkk8?entry=ttu"
           target="_blank"
           rel="noopener"
         >
