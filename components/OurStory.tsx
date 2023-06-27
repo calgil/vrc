f0c02717b2921e29601471d8af78a307b9451fc7
calgil <clgilbert1105@gmail.com>
2023-06-27 07:53:56 -0500

    about skeleton

--- components/OurStory.tsx
+++ components/OurStory.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export const OurStory = () => {
   return (
     <section id="our-story" className={s.container}>
-      <h3 className={s.headerText}>Our Story</h3>
       <div className={s.ourStory}>
         <div className={s.imgContainer}>
           <Image
@@ -19,16 +18,16 @@ export const OurStory = () => {
           />
         </div>
         <div className={s.storyText}>
-          <h5>
+          <h5 className={s.storyTitle}>
             North Springs Veterinary Referral Center is a locally owned 24-hour
             emergency and specialty center located in Colorado Springs.
           </h5>
-          <p>
+          <p className={s.story}>
             Our goal is to provide top-quality, innovative emergency and
             specialty veterinary care services to pet owners with empathy and
             transparency.
           </p>
-          <p>
+          <p className={s.story}>
             In 2021, our founding team realized that the existing emergency
             veterinary practices in the Colorado Springs area consistently had
             wait times of 3 – 6+ hours, even turning clients away. In addition,
@@ -37,14 +36,14 @@ export const OurStory = () => {
             emergency room on their worst days, both pets and owners experience
             high levels of stress and frustration.
           </p>
-          <p>
+          <p className={s.story}>
             What differentiates North Springs VRC is our commitment to providing
             the highest quality client experience from start to finish. Our goal
             is to provide excellent emergency and specialty care for our
             patients, exceptional service to our clients, and unprecedented
             support of our employees.
           </p>
-          <p>
+          <p className={s.story}>
             We look forward to supporting our community at 10520 White Diamond
             Point Colorado Springs, CO at the corner of Federal Drive and White
             Diamond Point!
