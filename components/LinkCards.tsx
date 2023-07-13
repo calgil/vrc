193580a639ddc72793c911570866cc500df407ce
calgil <clgilbert1105@gmail.com>
2023-07-13 06:57:33 -0600

    fix img paths

--- components/LinkCards.tsx
+++ components/LinkCards.tsx
@@ -12,25 +12,25 @@ export const LinkCards = () => {
   const linkCards: LinkCard[] = [
     {
       url: "/services#emergency",
-      bgUrl: "/home/cardBg/emergencyBg.webp",
+      bgUrl: "/pages/home/cardBg/emergencyBg.webp",
       bgColor: "rgba(154, 48, 64, 0.85)",
       text: "Emergency Services",
     },
     {
       url: "/services",
-      bgUrl: "/home/cardBg/specialtyBg.webp",
+      bgUrl: "/pages/home/cardBg/specialtyBg.webp",
       bgColor: "rgba(111, 113, 168, 0.85)",
       text: "Specialty Service",
     },
     {
       url: "/veterinarians",
-      bgUrl: "/home/cardBg/vets.webp",
+      bgUrl: "/pages/home/cardBg/vets.webp",
       bgColor: "rgba(37, 37, 37, 0.85)",
       text: "For Referring Veterinarians",
     },
     {
       url: "/veterinarians",
-      bgUrl: "/home/cardBg/owners.webp",
+      bgUrl: "/pages/home/cardBg/owners.webp",
       bgColor: "rgba(1, 109, 118, 0.85)",
       text: "For Pet Owners",
     },
