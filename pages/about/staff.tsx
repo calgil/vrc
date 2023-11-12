f164ada5352687aaf8189935193490689249417d
calgil <clgilbert1105@gmail.com>
2023-11-12 15:36:03 -0700

    start staff page

--- pages/about/staff.tsx
+++ pages/about/staff.tsx
@@ -1,7 +1,6 @@
-import { ComingSoon } from "@/components/ComingSoon";
 import { Hero } from "@/components/Hero";
 import { CustomMetadata, Meta } from "@/components/Meta";
-import Head from "next/head";
+import s from "../../styles/pages/Staff.module.scss";
 
 export default function Staff() {
   const customMetadata: CustomMetadata = {
@@ -14,9 +13,27 @@ export default function Staff() {
         <Hero
           homepage={false}
           bgUrl="/pages/about/staff/teamBanner.webp"
-          heading="Meet Our Staff"
+          heading="Meet Our Team"
+          subHeading="Our team is the best in the business!"
         />
-        <ComingSoon />
+        <section className={s.teamContainer}>
+          <h2 className={s.title}>Our Team</h2>
+          <p className={s.content}>
+            Our teams consists of dedicated professionals who work hard to be
+            experts in their craft. We use that expertise to bring healing and
+            confidence to you and your family.
+          </p>
+          <ul className={s.teamSelection}>
+            <li className={s.team}>Anesthesiology</li>
+            <li className={s.team}>Cardiology</li>
+            <li className={s.team}>Diagnostics</li>
+            <li className={s.team}>Emergency</li>
+            <li className={s.team}>Exotics</li>
+            <li className={s.team}>Internal Medicine</li>
+            <li className={s.team}>Rehabilitation</li>
+            <li className={s.team}>Surgery</li>
+          </ul>
+        </section>
       </main>
     </>
   );
